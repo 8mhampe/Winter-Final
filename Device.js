5f7b60b4e0066952b52fc5b527367f1cb2f89abd
8mhampe <31480929+8mhampe@users.noreply.github.com>
2017-12-13 10:10:32 -0600

    Add files via upload

--- Device.js
+++ Device.js
@@ -4,7 +4,7 @@ function Device(t,ma,c){
     //Instance Variables
     this.state = "off";
     this.type = t;
-    this.milliamps = ma;
+    this.milliAmps = ma;
     this.capacity = c;
     this.juice = 1;
     this.rate = [0.0015,0.0235,0.23];
@@ -40,33 +40,36 @@ function Device(t,ma,c){
     };
 
     this.use = function(min){
+      let time = min/60;
         if(this.state == "off"){
-            this.juice = this.juice - (this.rate[0]*min)
+            this.juice = this.juice - (this.rate[0]*time)
         }
         else if(this.state == "idle"){
-            this.juice = this.juice - (this.rate[1]*min)
+            this.juice = this.juice - (this.rate[1]*time)
         }
         else if(this.state == "active"){
-            this.juice = this.juice - (this.rate[2]*min)
+            this.juice = this.juice - (this.rate[2]*time)
         }
-        if()
+        if(this.juice<=0){
+          this.juice = 0;
+        }
     };
 
     this.charge = function(min){
         //adds more electricity to the device's juice depending on its state
-        let charge = (this.millAmps / this.capacity);
+        let charge = (this.milliAmps / this.capacity);
         let time = min / 60;
         let output;
-        if(this.state == "off"){
+          if(this.state == "off"){
             output = 1 - this.rate[0];
-        }
-        else if(this.state == "idle"){
+          }
+          else if(this.state == "idle"){
             output = 1 - this.rate[1];
-        }
-        else if(this.state == "active"){
+          }
+          else if(this.state == "active"){
             output = 1 - this.rate[2];
-        }
-        return output
+          }
+
         this.juice = this.juice + charge*output*time;
         //resets juice to 1 if it has exceeded 1
         if(this.juice > 1){
@@ -75,7 +78,6 @@ function Device(t,ma,c){
 
     };
 }//end of the device declaration
-
 //defines the testing code.
 function main(){
   let flemphone = new Device("phone",3000,10000);
